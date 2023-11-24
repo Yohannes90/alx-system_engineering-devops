2267f64cabe0c25dd698b59b8690384c5caf6f12
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-24 19:15:56 +0300

    fifth commit for question 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install flask from pip3, using Puppet
 
+package { 'Werkzeug':
+    ensure   => '2.2.2',
+    provider => 'pip3'
+}
+
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3'
