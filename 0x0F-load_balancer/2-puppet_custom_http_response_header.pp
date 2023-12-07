3b2cb02212411bee97263e4c2b6a3fe1ef9dacbf
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-12-07 12:56:08 +0300

    first commit for question 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# creating a custom HTTP header response, but with Puppet.
+#   The name of the custom HTTP header must be X-Served-By
+#   The value of the custom HTTP header must be the hostname of the server Nginx is running on
+
+exec { 'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec { 'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
