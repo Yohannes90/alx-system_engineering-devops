0e4d343853c299602ea0167aa3d428d5b15a8195
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-13 22:59:18 +0300

    third commit for question 0, correction returning 0 for 404 error only

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "linux: 0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        results = response.json().get("data")
-        return results.get("subscribers")
-    return 0
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
