3b7848dbf5419a733ae988b0cfc480b559419d98
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-13 23:18:20 +0300

    third commit for question 1, pycodestyle correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,9 +10,10 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {"User-Agent": "linux: 0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-    if response.status_code == 404:
+    if response.status_code != 200:
         print("None")
         return
     results = response.json().get("data")
