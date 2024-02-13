570560ebb5fbe35fe2a4a12eaba471a0822cb869
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-13 23:11:06 +0300

    second commit for question 1, correction on adding user agent to header

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,10 +8,11 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "linux: 0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"limit": 10}
-    response = requests.get(url, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 404:
         print("None")
         return
     results = response.json().get("data")
