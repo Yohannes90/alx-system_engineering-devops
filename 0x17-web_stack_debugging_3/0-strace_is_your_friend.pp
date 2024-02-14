b096a4c5582dde1bd2d7fcaf17111be641d1d873
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-14 13:50:10 +0300

    second commit for question 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # fixes wrong extention name 'phpp' to 'php' in wp-settings
 
 exec {'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path    => '/usr/local/bin/:/bin/'
+	provider => shell,
+	command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
+
