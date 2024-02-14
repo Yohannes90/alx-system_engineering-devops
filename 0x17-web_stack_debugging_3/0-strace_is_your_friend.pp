844e23daa8f760d7537a52d84fefd52752dd9126
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-14 13:54:54 +0300

    third commit for question 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,7 @@
 
 exec {'fix-wordpress':
 	provider => shell,
-	command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+	command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+	path     => '/bin';
 }
 
