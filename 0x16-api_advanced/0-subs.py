1d45cc6719c2dd6a5bd6d375cd4fa51440b8b58a
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-14 12:36:41 +0300

    commit after QA review for question 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ def number_of_subscribers(subreddit):
     """ return number of subscribers or 0 if invalid subredit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux: 0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    headers = {"User-Agent": "My-User-Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 404:
+    if response.status_code >= 300:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
